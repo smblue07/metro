221435c1c5ca77e594c7916ff7408cd857dae515
smblue07 <118044213+smblue07@users.noreply.github.com>
2026-06-17 13:51:29 +0330

    Initial commit

---
+++ components/pwa/index.ts
@@ -0,0 +1,10 @@
+/**
+ * PWA Components Index
+ *
+ * This file exports all PWA-related components for easy importing.
+ *
+ * @example
+ * import { InstallPrompt, InstallToast } from '@/components/pwa'
+ */
+
+export { InstallPrompt } from './install-prompt';
